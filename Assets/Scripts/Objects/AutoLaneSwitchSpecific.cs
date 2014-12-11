1da2fe541314f643b891597b4ff9f9d4e6d8c0b3
Charles <charles.bernardoff@gmail.com>
2014-12-11 18:34:34 +0100

    New component to handle specific behaviors like being ignored by automatic lane switching

---
+++ Assets/Scripts/Objects/AutoLaneSwitchSpecific.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoLaneSwitchSpecific : MonoBehaviour {
+
+	public bool absolutePriority = false;
+	public bool ignoredByAutoSwitch = false;
+}
